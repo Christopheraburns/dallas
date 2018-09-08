41873728036e4dfee3d17d2412560ac4be5c9385
chris <christopheraburns@gmail.com>
2018-09-08 14:29:10 -0500

    initial commit

---
+++ __main__.py
@@ -0,0 +1,24 @@
+import sys
+import computervision as vision
+
+
+
+def main(init):
+    try:
+        print("Script loaded")
+
+        # Take a picture
+
+        # Send to rekognition image-to-text service
+
+        # Get result
+
+        # Send result to Polly
+
+        # Get result
+
+        # Play audio file from Polly
+    except Exception as e:
+        print(e)
+if __name__ =="__main__":
+    main(True)
---
+++ computervision.py
@@ -0,0 +1,37 @@
+import pygame
+import pygame.camera
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+
+
+# Create an AWS Client Obj
+session = Session(profile_name="default")
+rekognition = session.client("rekognition")
+
+
+class Vision(object):
+
+    @staticmethod
+    def callRekognition():
+        try:
+            print("do something here")
+        except(BotoCoreError, ClientError) as e:
+            print("error: {}".format(e))
+
+
+    @staticmethod
+    def takeSinglePicture(detect_labels=False):
+        try:
+            pygame.camera.init()
+            cameras = pygame.camera.list_cameras()
+            for camera in cameras:
+                if camera:
+                    cam = pygame.camera.Camera(camera(640,480),"RGB")
+                    cam.start()
+                    img = cam.get_image()
+                    pygame.image.save(img,"capture.png")
+                    cam.stop()
+                break
+        except Exception as e:
+            print("error: {}".format(e))
+
