14e6a90b97fcf0dfda2cca2ec3b217dc2b4edb98
chris <christopheraburns@gmail.com>
2018-09-09 13:30:18 -0500

    Added call to Rekognition image-to-text service

--- __main__.py
+++ __main__.py
@@ -1,24 +1,21 @@
 import sys
 import computervision as vision
+import computerspeech as speech
 
 
 
 def main(init):
     try:
+
         # Take a picture
         vision.Vision.takeSinglePicture()
 
         # Send to rekognition image-to-text service
         result = vision.Vision.imageToText()
 
-        # Get result
-        print(result)
-
         # Send result to Polly
+        speech.pollySays(result)
 
-        # Get result
-
-        # Play audio file from Polly
     except Exception as e:
         print("Error in def main(): ".format(e))
 
---
+++ computerspeech.py
@@ -0,0 +1,29 @@
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+import os
+import pygame.mixer
+from contextlib import closing
+import time
+
+session = Session(profile_name="default")
+polly = session.client("polly")
+
+
+
+def pollySays(value):
+    try:
+        response = polly.synthesize_speech(Text=value, OutputFormat="ogg_vorbis", VoiceId="Emma")
+        if "AudioStream" in response:
+            with closing(response["AudioStream"]) as stream:
+                output = os.path.join("speech.ogg")
+
+                with open(output,"wb") as file:
+                    file.write(stream.read())
+
+            pygame.mixer.init()
+            pygame.mixer.music.load('speech.ogg')
+            pygame.mixer.music.play()
+            time.sleep(1)
+
+    except(BotoCoreError, ClientError) as e:
+        print("Error: computerspeech.py -> PollySays: {}".format(e))
